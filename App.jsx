3b8a8812b37cf98b89caab8a09cefb9e0dce34f3
Volodymyr Ok <v.shark.t@gmail.com>
2023-03-14 16:34:05 +0200

    started learning react native

---
+++ App.jsx
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Перше завдння виконано 👍</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
