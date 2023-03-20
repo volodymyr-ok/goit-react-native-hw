bb737d4b964f68b31f2b281a3800242ae34f4fe6
Volodymyr Ok <v.shark.t@gmail.com>
2023-03-20 23:18:27 +0200

    added simple logout route

--- Screens/HomeScreen/Home/Home.jsx
+++ Screens/HomeScreen/Home/Home.jsx
@@ -1,31 +1,57 @@
 import React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, Button, TouchableOpacity } from 'react-native';
 // CommentsScreen
 // MapScreen
 
-const Home = () => {
+const Home = ({ navigation }) => {
   return (
-    <View styles={styles.mainScreen}>
-      <Text style={styles.mainText}>HomeHomeHomeHomeHomeHome</Text>
-      <Text style={styles.mainText}>HomeHomeHomeHomeHomeHome</Text>
-      <Text style={styles.mainText}>HomeHomeHomeHomeHomeHome</Text>
-      <Text style={styles.mainText}>HomeHomeHomeHomeHomeHome</Text>
-      <Text style={styles.mainText}>HomeHomeHomeHomeHomeHome</Text>
-      <Text style={styles.mainText}>HomeHomeHomeHomeHomeHome</Text>
-      <Text style={styles.mainText}>HomeHomeHomeHomeHomeHome</Text>
-      <Text style={styles.mainText}>HomeHomeHomeHomeHomeHome</Text>
+    <View style={styles.mainScreen}>
+      <Text style={styles.mainText}>HOME</Text>
+
+      <TouchableOpacity style={styles.commonBtn} onPress={() => navigation.navigate('Login')}>
+        <Text style={styles.btnText}>logout</Text>
+      </TouchableOpacity>
+
+      <Button title="Posts" style={styles.commonBtn} onPress={() => navigation.navigate('Posts')} />
+
+      <Button
+        title="CreatePosts"
+        style={styles.commonBtn}
+        onPress={() => navigation.navigate('CreatePosts')}
+      />
+
+      <Button
+        title="back"
+        style={styles.commonBtn}
+        onPress={() => navigation.navigate('Profile')}
+      />
     </View>
   );
 };
 
+// borderColor: 'green',
+// borderWidth: 1,
+// { flex: 1, alignItems: 'center', justifyContent: 'center' }
+
 const styles = StyleSheet.create({
   mainScreen: {
     flex: 1,
-    justifyContent: 'flex-end',
-    alignItems: 'flex-end',
-    padding: 50,
+    justifyContent: 'center',
+    paddingHorizontal: 20,
   },
   mainText: {
+    fontFamily: 'Orbitron',
+    fontSize: 30,
+    marginVertical: 30,
+    textAlign: 'center',
+  },
+  commonBtn: {
+    marginBottom: 15,
+    padding: 15,
+    backgroundColor: '#28b3b6',
+    borderRadius: 15,
+  },
+  btnText: {
     textAlign: 'center',
   },
 });
